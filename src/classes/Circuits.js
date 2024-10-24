b35886f2e9bb4731cb67677e0dc2e2fd69e8e911
Your Name <186149954+OliverDevsCode@users.noreply.github.com>
2024-10-24 12:42:26 +0100

    Circuits Class and File Structure

---
+++ src/classes/Circuits.js
@@ -0,0 +1,38 @@
+class Circuits{
+    #circuitsId;
+    #name;
+    #location;
+    #country;
+    #countryCode;
+    #length
+
+    constructor(circuits_Data){
+    this.#circuitsId = circuits_Data.getColumn("circuitId")
+    this.#name = circuits_Data.getColumn('name')
+    this.#location = circuits_Data.getColumn('location')
+    this.#country = circuits_Data.getColumn('country')
+    this.#countryCode = circuits_Data.getColumn('countryCode')
+    this.#length = circuits_Data.getRowCount()
+
+    }
+
+    circuitsId(index){
+        return this.#circuitsId[index]
+    }
+    name(index){
+        return this.#name(index)
+    }
+    location(index){
+        return this.#location(index)
+    }
+    country(){
+        return this.#country(index)
+    }
+    countryCode(){
+        return this.#countryCode(index)
+    }
+    length(){
+        return this.#length
+    }
+
+}
