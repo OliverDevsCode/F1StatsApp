92b2f59a1f2e4c3d856453045e581e440af7b98e
Your Name <186149954+OliverDevsCode@users.noreply.github.com>
2024-11-05 14:56:11 +0000

    Simulation Foundation

---
+++ src/classes/Simulation.js
@@ -0,0 +1,13 @@
+class Simultation{
+    #drverA_ID;
+    #driverB_ID;
+    #season_data;
+    #races_left;
+
+    constructor(driverA_ID,driverB_ID,season_data,races_left){
+        this.#drverA_ID = driverA_ID;
+        this.#driverB_ID = driverB_ID;
+        this.#season_data = season_data;
+        this.#races_left = races_left;
+    }
+}
